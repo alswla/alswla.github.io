1e35c2037303299952bd183e06484cd3ec907f60
J <133668286+alswla@users.noreply.github.com>
2024-07-17 16:48:41 +0900

    Chore: add packages for code screen

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,4 +1,6 @@
 import { GatsbyBrowser } from "gatsby"
+import "prismjs/themes/prism-tomorrow.min.css"
+
 import Layout from "./src/components/common/Layout"
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
