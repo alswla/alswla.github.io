36cb55210796cab71b23c88b8f5009b17b25e003
J <133668286+alswla@users.noreply.github.com>
2024-07-18 19:42:32 +0900

    Refactor: add TableOfContent component and change CSS

--- src/components/post/PostBody/PostBody.styles.ts
+++ src/components/post/PostBody/PostBody.styles.ts
@@ -1,24 +1,22 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div`
-  width: 100%;
   position: relative;
-  /* display: grid; */
-  /* grid-template-columns: 1fr 220px; */
-  /* grid-gap: 30px; */
-  /* justify-content: space-between; */
-  /* align-items: flex-start; */
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 220px;
+  grid-gap: 30px;
+  justify-content: space-between;
+  align-items: flex-start;
+  /* justify-content: center; */
+  /* align-items: center; */
   padding-top: 100px;
 `
 export const Content = styled.div`
-  width: 100%;
   overflow: auto;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  /* align-items: center; */
+  /* justify-content: center; */
   gap: 100px;
   font-size: 16px;
   line-height: 2;
--- src/components/post/PostBody/index.tsx
+++ src/components/post/PostBody/index.tsx
@@ -5,6 +5,7 @@ import * as B from "./PostBody.styles"
 import "prismjs/components/prism-typescript"
 
 import Comment from "../Comment"
+import TableOfContents from "../TableOfContents"
 import useRenderRichText from "../../../hooks/useRenderRichText"
 
 type PostBodyProps = {
@@ -22,7 +23,7 @@ export default function PostBody({ content }: PostBodyProps) {
         <div id="content">{richText}</div>
         <Comment />
       </B.Content>
-      {/* 플로팅 목차 컴포넌트가 들어갈 자리 */}
+      <TableOfContents content={content} />
     </B.Wrapper>
   )
 }
